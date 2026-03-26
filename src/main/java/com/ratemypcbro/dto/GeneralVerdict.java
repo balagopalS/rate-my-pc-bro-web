3465d770203c00b132afe8a8e60ccd8664ede0d5
balagopalS <elithaxorprophetcrysis3@gmail.com>
2026-03-27 01:01:21 +0530

    edit the dtos to make them more modern

--- src/main/java/com/ratemypcbro/dto/GeneralVerdict.java
+++ src/main/java/com/ratemypcbro/dto/GeneralVerdict.java
@@ -1,33 +1,14 @@
 package com.ratemypcbro.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GeneralVerdict {
     private String rating;
     private String verdict;
     private String roast;
-
-    public GeneralVerdict() {}
-
-    public GeneralVerdict(String rating, String verdict, String roast) {
-        this.rating = rating;
-        this.verdict = verdict;
-        this.roast = roast;
-    }
-
-    public String getRating() { return rating; }
-    public void setRating(String rating) { this.rating = rating; }
-
-    public String getVerdict() { return verdict; }
-    public void setVerdict(String verdict) { this.verdict = verdict; }
-
-    public String getRoast() { return roast; }
-    public void setRoast(String roast) { this.roast = roast; }
-
-    @Override
-    public String toString() {
-        return "GeneralVerdict{" +
-                "rating='" + rating + '\'' +
-                ", verdict='" + verdict + '\'' +
-                ", roast='" + roast + '\'' +
-                '}';
-    }
 }
